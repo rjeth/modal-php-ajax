1a5fac6033fd4d758296d13576d75e075f0714eb
ジェスロ・ジョロラン <40770876+rjeth@users.noreply.github.com>
2022-01-15 15:07:24 +0800

    update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_SERVER', 'localhost:3306');
   define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', 'P@ssw0rd');
+  define('DB_PASSWORD', '');
   define('DB_NAME', 'mulitple_file');
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
